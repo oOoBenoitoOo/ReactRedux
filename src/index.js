ff5489a876168296af3d5092b8d240ac22e92cd8
Benoit <nau.benoit@gmail.com>
2023-03-16 15:17:37 -0400

    Redux configuration

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from "react";
 import { render } from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import App from "./components/App";
+import configureStore from "./redux/configureStore";
+import { Provider as ReduxProvider } from "react-redux";
 
-function Hi() {
-  return <p>Hi</p>;
-}
-
-render(<Hi />, document.getElementById("app"));
+const store = configureStore();
+render(
+  <ReduxProvider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </ReduxProvider>,
+  document.getElementById("app")
+);
